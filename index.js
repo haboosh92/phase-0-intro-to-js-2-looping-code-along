9a889470ef1b7fd1cada299a51a07d92b93e0709
Hiba <eng.lilil92@gmail.com>
2022-10-19 02:42:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+function writeCards(array,event)
+{
+    let newA = [];
+    for (let i=0;i<array.length;i++)
+    {
+        newA[i]=`Thank you, ${array[i]}, for the wonderful ${event} gift!`
+    
+    }
+    
+    return newA;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "birthday");
+
+
+function countDown(i) {
+    if (i>0)
+    {
+        while (i>=0)
+        {
+            console.log(i)
+            i=i-1;
+        }
+    }
+}
+
